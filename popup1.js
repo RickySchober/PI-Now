2902061a2e7fa3cb23227451b2510b4e38e8380c
RickySchober <93233344+RickySchober@users.noreply.github.com>
2023-10-30 16:41:00 -0700

    oops wrong file

--- popup1.js
+++ popup1.js
@@ -1,13 +1,37 @@
 
-const addResponseElement = (element) => {
+const addResponseElement = (data = [], element) => {
     const newSearchResponse = document.createElement("div");
-    newSearchResponse.textContent = "yo";
     newSearchResponse.className = "searchResponse";
-    response.appendChild(newSearchResponse);
+
+    const title = document.createElement("div");
+    title.className = "responseTitle";
+    title.textContent = data[4]; //Product Name
+    newSearchResponse.appendChild(title);
+
+    const price = document.createElement("div");
+    price.className = "responsePrice";
+    price.textContent = data[0]; //Product Price
+    newSearchResponse.appendChild(price);
+
+    const url = document.createElement("a");
+    url.className = "responseURL";
+    url.href = data[2];
+    url.textContent = "link";
+    url.target = "_blank";
+    newSearchResponse.appendChild(url);
+
+    element.appendChild(newSearchResponse);
 }
+ 
 const addResponse = (data, resultElement) => {
-    for(let  i = 0; i<jsonSize; i++){
-        addResponseElement(data[i], resultElement);
+    resultElement.removeChild(resultElement.firstChild);
+    for (const [key, value] of Object.entries(data)){
+        arr = [];
+        for (x in value[x]){
+            arr.push(value[x]);
+        }
+        arr.push(key);
+        addResponseElement(arr, resultElement);
     }
 }
 
@@ -20,8 +44,10 @@ document.getElementById("search-button").addEventListener("click", () =>{
     newSearchResponse.textContent = "awaiting fetch response...";
     newSearchResponse.className = "wait message";
     responseElement.appendChild(newSearchResponse);
-
-    fetch('http://localhost:5000/data')
+    //test elements 
+    //const temp = ["Gucci Bag","$6969","https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjuhP_08J6CAxUxzcIEHUUhCAsYABAAGgJwdg&gclid=Cj0KCQjwqP2pBhDMARIsAJQ0CzrVeFDSLlQWR2YnQt5mlDiOObxMihLnYL2-862g96XzCIe1nixNC7gaAgtKEALw_wcB&ohost=www.google.com&cid=CAESVuD2i-rr6Lmln0cKgW0VcBAdM2oaUUJ8lIT4HOAJij8vM4CA_mvsH8XeF2lGY6rtCeuAmz8SbGFouMDJMLHZMgRrgccz9xBfKbSA2XrMbnh-szpcJVTo&sig=AOD64_1sfhtk6FQdnVqapcIcE2ejNorTqA&q&adurl&ved=2ahUKEwjk0PT08J6CAxWaI0QIHRTnCyMQ0Qx6BAgOEAE&nis=2"]
+    //addResponseElement(temp, responseElement)
+    fetch('http://localhost:5000/data', {method:'GET',mode:'no-cors'})
     .then(response => {
         if (!response.ok) {
             throw new Error('Network response was not ok');
